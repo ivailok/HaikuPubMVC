d4e0c76b09ded98c4997fffdc8d401bee3fb1cf3
Ivaylo Kirilov <ivailokirilov93@gmail.com>
2016-01-12 15:18:37 +0200

    Styled haikus view

---
+++ Haiku.Web/Scripts/autoHeight.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    var txtArea = $("textarea");
+    var resize = function () {
+        txtArea.scrollTop = txtArea.scrollHeight;
+        txtArea.style.height = "" + txtArea.scrollHeight + "px";
+    };
+    txtArea.on("input change", resize);
+    $timeout(resize, 0);
+});
